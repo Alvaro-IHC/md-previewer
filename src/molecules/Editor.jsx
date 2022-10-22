8db5613e3dfa12514918e60af1ef4a0877ca64d2
Alvaro-IHC <alva0741@gmail.com>
2022-10-22 18:08:05 -0400

    Modified: Editor and Previewer complete

--- src/molecules/Editor.jsx
+++ src/molecules/Editor.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Editor = ({ expanded }) => {
+const Editor = ({ expanded, value, handleChange }) => {
   return (
-    <div id="editor" className={`${expanded ? "expanded" : ""}`}>
-      <textarea />
+    <div className={`${expanded ? "expanded" : ""}`}>
+      <textarea
+        id="editor"
+        value={value}
+        onChange={(event) => {
+          handleChange(event.target.value);
+        }}
+      />
     </div>
   );
 };
--- src/molecules/Previewer.jsx
+++ src/molecules/Previewer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-const Previewer = ({ expanded }) => {
+const Previewer = ({ expanded, value }) => {
   return (
-    <div id="previewer" className={`${expanded && "expanded"}`}>
-      <div className="content">Hola</div>
+    <div id="preview" className={`content ${expanded ? "expanded": ""}`}>
+        {/* <ReactMarkdown>{value}</ReactMarkdown> */}
+        <ReactMarkdown children={value} remarkPlugins={[remarkGfm]} />
     </div>
   );
 };
